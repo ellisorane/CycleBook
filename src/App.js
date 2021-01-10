fbd81b905fd15a6a607f9a01d76c5841860d02b6
ellisorane <ellisorane@yahoo.com>
2021-01-10 16:01:33 -0500

    Changed folder name from app to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
-import data from './app/data/feedData'
+import data from './components/data/feedData'
 
-import { Navbar } from './app/Navabar/Navbar'
-import Feed from './app/Feed/Feed'
+import { Navbar } from './components/Navabar/Navbar'
+import Feed from './components/Feed/Feed'
 
 function App() {
-  //Create users state amd setState
+  //Create users state and setState
   const [users, setUsers] = useState(data)
     return (
       <div className="App">
